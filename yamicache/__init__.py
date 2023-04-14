f9d3df62e251819d14009e9f03d2b00415b90fe4
Timothy McFadden <mtik00@users.noreply.github.com>
2023-04-14 11:39:14 -0600

    [skip ci] prep for release

--- yamicache/__init__.py
+++ yamicache/__init__.py
@@ -3,6 +3,6 @@
 """Top-level package for yamicache."""
 
 __author__ = """Timothy McFadden"""
-__version__ = '0.7.1'
+__version__ = '0.7.2'
 
 from .yamicache import Cache, nocache, override_timeout  # noqa: F401
